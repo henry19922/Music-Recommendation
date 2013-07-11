d9469739770e85b7a7841b5fc48ba840b3a1f7ef
henry19922 <wls461532792@gmail.com>
2013-07-11 21:07:57 +0800

    给php添加注释

--- config.php
+++ config.php
@@ -1,6 +1,9 @@
 <?php
 header('Content-Type: text/html; charset=UTF-8');
 
+#应用key
 define( "WB_AKEY" , '3237955472' );
+#安全key
 define( "WB_SKEY" , '77d554b65ea54d1990abb56fce8d67a0' );
+#定义回调页面
 define( "WB_CALLBACK_URL" , 'http://192.168.235.90/Competition_Computer/callback.php' );
